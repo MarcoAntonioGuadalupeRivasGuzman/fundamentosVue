e507ddfe247d7a0527396d85bf15b7329bbc1c68
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-03-03 12:20:14 -0600

    se creo una funcion para incrementar el valor de contador y para decrementar el valor del contador

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,6 +1,10 @@
 <template>
     <h2>Counter</h2>
     <p>{{ counter }} <sup>2</sup> = {{ squeareCounter }} </p>
+    <div>
+        <button @click="incrementCounter()" class="btn">+1</button>
+        <button @click="decrementCounter()" class="btn">-1</button>
+    </div>
 </template>
 
 <script>
@@ -13,6 +17,12 @@ export default {
     methods:{
         getSquareValue(){
             return this.counter * this.counter
+        },
+        incrementCounter(){
+            return this.counter = this.counter +1;
+        },
+        decrementCounter(){
+            return this.counter = this.counter -1;
         }
     },
     computed: {
@@ -25,5 +35,19 @@ export default {
 </script>
 
 <style>
+.btn{
+    background-color: #64BB87;
+    border-radius: .5rem;
+    border: 1px solid white;
+    cursor: pointer;
+    color: white;
+    padding: 5px 15px;
+    margin: 1rem;
+    transition: 0.3s ease-in-out;
+}
 
+.btn:hover{
+    background-color: #5aa67b;
+    transition: 0.3s ease-in-out;
+}
 </style>
